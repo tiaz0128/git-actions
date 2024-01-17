69bb08df8f83aa97d4f4b0e6448290db5327c1ff
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-17 11:06:03 +0900

    fix

--- script/read_json.py
+++ script/read_json.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     key = sys.argv[1]
 
-    with open("solutions-info.json", encoding="utf-8") as fp:
+    with open("solutions.json", encoding="utf-8") as fp:
         solutions = json.load(
             fp,
         )
