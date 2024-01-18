fe08d3ef4b33aea8025aa4f1b7810a07d6ece98b
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-19 08:01:15 +0900

    fix

--- script/read_pass_solution.py
+++ script/read_pass_solution.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
 
     # ..\\src\\tiaz0128\\ch_05\\solution_001.py
     with open(f"..\\{path}", encoding="utf-8") as fp:
-        print(fp.readline())
+        content = fp.read()
+        print(content, end="")
