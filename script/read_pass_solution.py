75e28a817d13bd82c100ec0915de302631cf94b3
통붕이 <tiaz0128.dev@gmail.com>
2024-01-19 09:08:25 +0900

    Update read_pass_solution.py

--- script/read_pass_solution.py
+++ script/read_pass_solution.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     path = sys.argv[1]
 
-    with open(f"..{path}", encoding="utf-8") as fp:
+    with open(f"{path}", encoding="utf-8") as fp:
         content = fp.read()
         print(content, end="")
