9db2e888d56e7e6ee6473602ff8fb60842e89d6c
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-25 18:45:00 +0900

    fix

--- script/update_readme.py
+++ script/update_readme.py
@@ -1,5 +1,6 @@
 from github import Github
 import sys
+import re
 
 from get_pass_pr import get_pass_label_count
 
@@ -10,15 +11,19 @@ def update_readme(token, repo_full_name):
 
     # README.md 파일을 가져옵니다.
     readme_file = repo.get_contents("README.md")
+    readme_content = readme_file.decoded_content.decode("utf-8")
 
     # 새로운 데이터를 받아옵니다.
     pass_label_counts = get_pass_label_count(token, repo_full_name)
 
-    # 기존 데이터를 삭제하고 새로운 데이터로 채웁니다.
-    new_content = "<!-- PR Status Start -->\n"
-    for user, count in pass_label_counts.items():
-        new_content += f"Pass count (auth_id): {user}\nPass count: {count} / 85\n"
-    new_content += "<!-- PR Status End -->\n"
+    # <!-- PR Status Start -->와 <!-- PR Status End --> 사이의 부분을 새로운 데이터로 교체합니다.
+    pattern = r"<!-- PR Status Start -->(.*?)<!-- PR Status End -->"
+    new_content = re.sub(
+        pattern,
+        f"<!-- PR Status Start -->\n{get_updated_data(pass_label_counts)}\n<!-- PR Status End -->",
+        readme_content,
+        flags=re.DOTALL,
+    )
 
     # README.md 파일을 업데이트합니다.
     repo.update_file(
@@ -27,6 +32,13 @@ def update_readme(token, repo_full_name):
         content=new_content,
         sha=readme_file.sha,
     )
+
+
+def get_updated_data(pass_label_counts):
+    updated_data = ""
+    for user, count in pass_label_counts.items():
+        updated_data += f"Pass count (auth_id): {user}\nPass count: {count} / 85\n"
+    return updated_data
 
 
 if __name__ == "__main__":
